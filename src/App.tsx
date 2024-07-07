9fec6577011f97d9cdcb07922c389561b6276cd3
Sheri Richardson <shericodes@protonmail.com>
2024-07-07 14:50:48 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <h1 className="text-center text-4xl font-bold underline">
+      Vite, React, TypeScript, and Tailwind starter
+    </h1>
+  )
+}
+
+export default App
